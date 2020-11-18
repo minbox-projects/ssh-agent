5fdd0b9269dc8dd770b09efca219fb95d340986c
hengboy <jnyuqy@gmail.com>
2020-11-18 15:38:17 +0800

    :white_check_mark: 添加用户名密码方式示例

--- src/test/java/AgentTest.java
+++ src/test/java/AgentTest.java
@@ -16,13 +16,28 @@ public class AgentTest {
      * 本地 "3307" 端口号绑定远程服务器 "3306"端口号，实现本地访问远程服务器上的MySQL服务
      */
     static void sshPrivateConnect() {
-        AgentConfig config2 = new AgentConfig();
-        config2.setServerIp("xxx.xxx.xxx.xxx");
-        config2.setUsername("root");
-        config2.setLocalPort(3307);
-        config2.setForwardTargetPort(3306);
-        AgentConnection connection2 = new DefaultAgentConnection(config2);
-        connection2.connect();
+        AgentConfig config = new AgentConfig();
+        config.setServerIp("xxx.xxx.xxx.xxx");
+        config.setUsername("root");
+        config.setLocalPort(3307);
+        config.setForwardTargetPort(3306);
+        AgentConnection connection = new DefaultAgentConnection(config);
+        connection.connect();
+        // 连接成功后，访问本地3307的数据库，其实就是访问远程服务器的3306
+    }
+
+    /**
+     * 使用用户名密码方式连接
+     */
+    static void usernamePasswordConnect() {
+        AgentConfig config = new AgentConfig();
+        config.setServerIp("xxx.xxx.xxx.xxx");
+        config.setUsername("root");
+        config.setPassword("密码");
+        config.setLocalPort(3307);
+        config.setForwardTargetPort(3306);
+        AgentConnection connection = new DefaultAgentConnection(config);
+        connection.connect();
         // 连接成功后，访问本地3307的数据库，其实就是访问远程服务器的3306
     }
 
